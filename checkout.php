8b10d40c0e52289136eedb5f68b9ea45b6f49888
Olivia-Steed <148646094+delightfulhare@users.noreply.github.com>
2025-06-11 02:31:09 +0000

    finished php/html form

---
+++ checkout.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+        <title>BTA3OV - 4.5 Assessment: Five Page Web Design</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <?php
+        $customer = "";
+        if ( isset( $_POST['userName'] ) ){
+        $userName = $_POST['userName'];
+        }
+        echo "<h1>Thank you for your purchase ".$userName."!</h1>\n";
+        echo "<p> You are order #1 </p>\n";
+        echo "<p> You will recieve a confirmation email with your purchase</p>\n";
+        echo "<p> Enjoy your flowers!</p>\n";
+        ?>
+    </body>
+</html>
