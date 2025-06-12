c95b5bd68ce480c7bc032c50428b105e5512eac4
Olivia-Steed <148646094+delightfulhare@users.noreply.github.com>
2025-06-12 17:41:10 +0000

    done!

--- checkout.php
+++ checkout.php
@@ -14,9 +14,9 @@
         $fname = $_POST['fname'];
         }
         echo "<h1 id='thank_you'>Thank you for your purchase ".$fname."!</h1>\n";
-        echo '<p id="order"> You are order #1 </p>\n';
-        echo '<p id="order"> You will recieve a confirmation email with your purchase</p>\n';
-        echo '<p id="order"> Enjoy your flowers!</p>\n';
+        echo "<p id='order'> You are order #1 </p>\n";
+        echo "<p id='order'> You will recieve a confirmation email with your purchase</p>\n";
+        echo "<p id='order'> Enjoy your flowers!</p>\n";
         ?>
     </body>
 </html>
